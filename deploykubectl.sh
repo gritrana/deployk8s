adca68a20966afbacee8bfe1b9e8c6a05e532950
xujintao <xujintao@126.com>
2018-07-28 11:07:10 +0800

    kubectl dispatch locally

--- deploykubectl.sh
+++ deploykubectl.sh
@@ -27,29 +27,29 @@ cfssl gencert \
   -ca-key=ca-key.pem \
   -config=ca-config.json \
   -profile=kubernetes admin-csr.json | cfssljson -bare admin
-ls admin*
+ls admin*.pem
+
+# 分发kubectl
+sudo cp kubernetes/client/bin/kubectl /usr/local/bin/
 
 # 创建kubeconfig文件
 # 设置集群参数
 kubectl config set-cluster kubernetes \
   --certificate-authority=ca.pem \
-  --server=${KUBE_APISERVER} \
-  --kubeconfig=kubectl.kubeconfig
+  --server=${KUBE_APISERVER}
 
 # 设置客户端认证参数
 kubectl config set-credentials admin \
   --client-certificate=admin.pem \
-  --client-key=admin-key.pem \
-  --kubeconfig=kubectl.kubeconfig
+  --client-key=admin-key.pem 
 
 # 设置上下文参数
 kubectl config set-context kubernetes \
   --cluster=kubernetes \
-  --user=admin \
-  --kubeconfig=kubectl.kubeconfig
+  --user=admin
 
 # 设置默认上下文
-kubectl config use-context kubernetes --kubeconfig=kubectl.kubeconfig
+kubectl config use-context kubernetes
 
 : '
 没有必要把kubectl部署到集群中
@@ -58,7 +58,8 @@ for master_node_ip in ${MASTER_NODE_IPS[@]}
   do
     echo ">>> ${master_node_ip}"
     echo "分发kubectl"
-    ssh k8s@${master_node_ip} "sudo mkdir -p /opt/k8s/bin && sudo chown -R k8s:k8s /opt/k8s"
+    ssh k8s@${master_node_ip} "sudo mkdir -p /opt/k8s/bin
+                               sudo chown -R k8s:k8s /opt/k8s"
     scp kubernetes/client/bin/kubectl k8s@${master_node_ip}:/opt/k8s/bin/
 
     echo "分发kubectl证书及私钥"
@@ -69,3 +70,5 @@ for master_node_ip in ${MASTER_NODE_IPS[@]}
     scp kubectl.kubeconfig k8s@${master_node_ip}:~/.kube/config
   done
 '
+
+
