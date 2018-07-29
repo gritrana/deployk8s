51a0bfd355359bb7d0de378f6581b8ee33ebe7d7
xujintao <xujintao@126.com>
2018-07-29 12:45:43 +0800

    add controller-manager deployment

---
+++ deploycontrollermanager.sh
@@ -0,0 +1,155 @@
+source env.sh
+
+# 创建controller-manager证书签名请求
+echo "==========创建controller-manager证书签名请求=========="
+cat > kube-controller-manager-csr.json <<EOF
+{
+    "CN": "system:kube-controller-manager",
+    "hosts": [
+        "127.0.0.1",
+        "$MASTER_IPS[0]",
+        "$MASTER_IPS[1]",
+        "$MASTER_IPS[2]"
+    ],    
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+            "C": "CN",
+            "ST": "BeiJing",
+            "L": "BeiJing",
+            "O": "system:kube-controller-manager",
+            "OU": "4Paradigm"
+        }
+    ]
+}
+EOF
+ls kube-controller-manager-csr.json
+
+# 生成controller-manager证书和私钥
+echo "========生成controller-manager证书和私钥========"
+cfssl gencert\
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-controller-manager-csr.json\
+  | cfssljson -bare kube-controller-manager
+ls kube-controller-manager*.pem
+
+# 创建controller-manager kubeconfig文件
+echo "==========创建controller-manager kubeconfig文件=========="
+kubectl config set-cluster kubernetes \
+  --certificate-authority=/etc/kubernetes/cert/ca.pem \
+  --server=${KUBE_APISERVER} \
+  --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-credentials system:kube-controller-manager \
+  --client-certificate=/etc/kubernetes/cert/kube-controller-manager.pem \
+  --client-key=/etc/kubernetes/cert/kube-controller-manager-key.pem \
+  --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-context system:kube-controller-manager \
+  --cluster=kubernetes \
+  --user=system:kube-controller-manager \
+  --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config use-context system:kube-controller-manager \
+  --kubeconfig=kube-controller-manager.kubeconfig
+
+# 创建controller-manager systemd unit文件
+echo "=========创建controller-manager systemd unit文件========="
+cat > kube-controller-manager.service <<EOF
+[Unit]
+Description=Kubernetes Controller Manager
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+
+[Service]
+ExecStart=/opt/k8s/bin/kube-controller-manager \\
+  --port=0 \\
+  --secure-port=10252 \\
+  --bind-address=127.0.0.1 \\
+  --kubeconfig=/etc/kubernetes/kube-controller-manager.kubeconfig \\
+  --service-cluster-ip-range=${SERVICE_CIDR} \\
+  --cluster-name=kubernetes \\
+  --cluster-signing-cert-file=/etc/kubernetes/cert/ca.pem \\
+  --cluster-signing-key-file=/etc/kubernetes/cert/ca-key.pem \\
+  --experimental-cluster-signing-duration=8760h \\
+  --root-ca-file=/etc/kubernetes/cert/ca.pem \\
+  --service-account-private-key-file=/etc/kubernetes/cert/ca-key.pem\\
+  --leader-elect=true \\
+  --feature-gates=RotateKubeletServerCertificate=true \\
+  --controllers=*,bootstrapsigner,tokencleaner \\
+  --horizontal-pod-autoscaler-use-rest-clients=true \\
+  --horizontal-pod-autoscaler-sync-period=10s \\
+  --tls-cert-file=/etc/kubernetes/cert/kube-controller-manager.pem \\
+  --tls-private-key-file=/etc/kubernetes/cert/kube-controller-manager-key.pem \\
+  --use-service-account-credentials=true \\
+  --alsologtostderr=true \\
+  --logtostderr=false \\
+  --log-dir=/var/log/kubernetes \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+User=k8s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+ls kube-controller-manager.service
+
+# 分发controller-manager及启动
+echo "=========分发controller-manager及启动========="
+for master_ip in ${MASTER_IPS[@]}
+  do
+    echo ">>> ${master_ip}"
+    echo "分发controller-manager二进制"
+    ssh k8s@${master_ip} "sudo mkdir -p /opt/k8s/bin
+                          sudo chown -R k8s:k8s /opt/k8s"
+    ssh k8s@${master_ip} \
+      "if [ -f /opt/k8s/bin/kube-controller-manager ];then
+       sudo systemctl stop kube-controller-manager
+       rm -f /opt/k8s/bin/kube-controller-manager
+       fi"
+    scp kubernetes/server/bin/kube-controller-manager \
+      k8s@${master_ip}:/opt/k8s/bin/
+
+    echo "分发证书和私钥"
+    ssh k8s@${master_ip} "sudo mkdir -p /etc/kubernetes/cert
+                          sudo chown -R k8s:k8s /etc/kubernetes"
+    scp kube-controller-manager*.pem \
+      k8s@${master_ip}:/etc/kubernetes/cert/
+
+    echo "分发kubeconfig文件"
+    scp kube-controller-manager.kubeconfig \
+      k8s@${master_ip}:/etc/kubernetes/
+
+    echo "分发systemd unit文件"
+    scp kube-controller-manager.service \
+      root@${master_ip}:/usr/lib/systemd/system/
+
+    echo "启动kube-controller-manager服务"
+    ssh k8s@${master_ip} "sudo mkdir -p /var/log/kubernetes
+                          sudo chown -R k8s:k8s /var/log/kubernetes"
+#                          sudo mkdir -p /var/run/kubernetes
+#                          sudo chown -R k8s:k8s /var/run/kubernetes
+    ssh k8s@${master_ip} \
+      "sudo systemctl daemon-reload
+       sudo systemctl enable kube-controller-manager
+       sudo systemctl start kube-controller-manager
+       sudo systemctl status kube-controller-manager | grep Active
+       sudo netstat -lnpt | grep kube-con"
+
+    echo "查看metric"
+    curl -s \
+         --cacert /etc/kubernetes/cert/ca.pem \
+         https://127.0.0.1:10252/metrics | head
+  done
+
+# 查看当前的leader
+echo "========查看当前的leader========="
+kubectl get endpoints kube-controller-manager \
+  --namespace=kube-system \
+  -o yaml
