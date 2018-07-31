ef9b0a40cbdd74751f13f034084df112f5432947
xujintao <xujintao@126.com>
2018-07-31 19:14:51 +0800

    disable swap

--- deployenv.sh
+++ deployenv.sh
@@ -1,10 +1,26 @@
 source ~/env.sh
 
 # 设置集群环境
-echo "=========设置所有机器环境========="
-for master_node_ip in ${MASTER_NODE_IPS[@]}
+echo "=========设置master机器环境========="
+for master_ip in ${MASTER_IPS[@]}
   do
-    echo ">>> ${master_node_ip}"
-    ssh k8s@${master_node_ip} "sudo setsebool -P haproxy_connect_any=1
-                               sudo systemctl disable firewalld"
+    echo ">>> ${master_ip}"
+    echo "永久关闭firewalld"
+    ssh k8s@${master_ip} "sudo systemctl disable firewalld"
+
+    echo "selinux永久使能haproxy"
+    ssh k8s@${master_ip} "sudo setsebool -P haproxy_connect_any=1"
+  done
+
+echo "=========设置node机器环境========="
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    echo "永久关闭firewalld"
+    ssh k8s@${node_ip} "sudo systemctl disable firewalld"
+
+    echo "永久关闭swap分区"
+    ssh k8s@${node_ip} "sudo /usr/sbin/swapoff -a
+                        sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' \
+                        /etc/fstab"
   done
