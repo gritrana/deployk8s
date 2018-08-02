320d493cebe8e0b2583f0514621a04c40bafb799
xujintao <xujintao@126.com>
2018-08-02 18:00:40 +0800

    mv some environment configuration to Vagrantfile

--- deployenv.sh
+++ deployenv.sh
@@ -1,37 +1,20 @@
-source ~/env.sh
+source env.sh
 
 # 设置集群环境
 echo "=========设置master机器环境========="
-for master_ip in ${MASTER_IPS[@]}
+for ((i=0; i<3; i++))
   do
-    echo ">>> ${master_ip}"
-    echo "yum安装keepalived和haproxy"
-    yum install -y keepalived haproxy
-
-    echo "永久关闭firewalld"
-    ssh root@${master_ip} "systemctl disable firewalld"
-
-    echo "selinux永久使能haproxy"
-    ssh root@${master_ip} "setsebool -P haproxy_connect_any=1"
+    echo ">>> ${MASTER_IPS[i]}"
+    echo "修改hosts，追加${MASTER_IPS[i]} ${MASTER_NAMES[i]}"
+    ssh root@${MASTER_IPS[i]} \
+      "echo '${MASTER_IPS[i]} ${MASTER_NAMES[i]}' >> /etc/hosts"
   done
 
 echo "=========设置node机器环境========="
-for node_ip in ${NODE_IPS[@]}
+for ((i=0; i<3; i++))
   do
-    echo ">>> ${node_ip}"
-    echo "yum安装docker"
-    yum install -y yum-utils \
-      device-mapper-persistent-data lvm2
-    yum-config-manager --add-repo \
-      https://download.docker.com/linux/centos/docker-ce.repo
-    yum install docker-ce-18.03.0.ce
-
-    echo "永久关闭firewalld"
-    ssh root@${node_ip} "systemctl disable firewalld"
-
-    echo "永久关闭swap分区"
-    ssh root@${node_ip} "/usr/sbin/swapoff -a
-                         cp /etc/fstab /etc/fstab.bak
-                         sed -i '/ swap / s/^\(.*\)$/#\1/g' \
-                         /etc/fstab"
+    echo ">>> ${NODE_IPS[i]}"
+    echo "修改hosts，追加${NODE_IPS[i]} ${NODE_NAMES[i]}"
+    ssh root@${NODE_IPS[i]} \
+      "echo '${NODE_IPS[i]} ${NODE_NAMES[i]}' >> /etc/hosts"
   done
