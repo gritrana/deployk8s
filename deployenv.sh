313c24364b61f196a18cb01cfe839b7663f01d7d
xujintao <xujintao@126.com>
2018-08-02 19:44:09 +0800

    config hosts

--- deployenv.sh
+++ deployenv.sh
@@ -5,16 +5,26 @@ echo "=========设置master机器环境========="
 for ((i=0; i<3; i++))
   do
     echo ">>> ${MASTER_IPS[i]}"
-    echo "修改hosts，追加${MASTER_IPS[i]} ${MASTER_NAMES[i]}"
-    ssh root@${MASTER_IPS[i]} \
-      "echo '${MASTER_IPS[i]} ${MASTER_NAMES[i]}' >> /etc/hosts"
+    for ((j=0; j<3; j++))
+      do
+        echo "修改hosts，追加${MASTER_IPS[j]} ${MASTER_NAMES[j]}"
+        ssh root@${MASTER_IPS[i]} \
+          "echo '${MASTER_IPS[j]} ${MASTER_NAMES[j]}' >> /etc/hosts"
+#          "if [ -z `awk '/$MASTER_IPS[j] $MASTER_NAMES[j]/' /etc/hosts` ];then
+#           echo '${MASTER_IPS[j]} ${MASTER_NAMES[j]}' >> /etc/hosts
+#           fi"
+      done
   done
 
 echo "=========设置node机器环境========="
 for ((i=0; i<3; i++))
   do
     echo ">>> ${NODE_IPS[i]}"
-    echo "修改hosts，追加${NODE_IPS[i]} ${NODE_NAMES[i]}"
-    ssh root@${NODE_IPS[i]} \
-      "echo '${NODE_IPS[i]} ${NODE_NAMES[i]}' >> /etc/hosts"
+    for ((j=0; j<3; j++))
+      do
+        echo "修改hosts，追加${NODE_IPS[j]} ${NODE_NAMES[j]}"
+        ssh root@${NODE_IPS[i]} \
+          "echo '${NODE_IPS[j]} ${NODE_NAMES[j]}' >> /etc/hosts"
+      done
   done
+
