25f730930ad3a377965688a6a808dcc0384f7efb
xujintao <xujintao@126.com>
2018-08-02 23:36:46 +0800

    add config.json

--- deployapp.sh
+++ deployapp.sh
@@ -1,5 +1,38 @@
 source env.sh
 
+# 编写testgin的配置文件
+cat > config.json <<EOF
+{
+    "db":{
+        "name": "mysql",
+        "user": "root",
+        "password": "1234",
+        "ip": "127.0.0.1",
+        "port": 3306,
+        "table":"test"
+    },
+
+    "etcd":{
+        "ip": "192.168.6.201",
+        "port": 2379
+    },
+
+    "serverA":{
+        "ip": "192.168.6.200",
+        "port": 1701
+    }
+}
+EOF
+ls config.json
+
+# 分发testgin配置文件
+for node_ip in ${NODE_IPS[@]}
+  do
+    echo ">>> ${node_ip}"
+    ssh root@${node_ip} "mkdir -p /root/testgin"
+    scp config.json root@${node_ip}:/root/testgin/
+  done
+
 # 编写testgin部署yaml
 echo "========编写testgin部署yaml========"
 cat > testgin.yaml << EOF
@@ -51,14 +84,6 @@ spec:
     app: web
 EOF
 ls testgin.yaml
-
-# 分发testgin配置文件
-for node_ip in ${NODE_IPS[@]}
-  do
-    echo ">>> ${node_ip}"
-    ssh root@${node_ip} "mkdir -p /root/testgin"
-    scp config.json root@${node_ip}:/root/testgin/
-  done
 
 # 创建testgin部署并暴露服务
 echo "========创建testgin部署并暴露服务========"
