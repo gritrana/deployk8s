88d2db36f3d19a433509cfbd25702673da3a7776
xujintao <xujintao@126.com>
2018-08-02 17:52:36 +0800

    config cluster ip

--- env.sh
+++ env.sh
@@ -15,22 +15,22 @@ CLUSTER_CIDR="172.30.0.0/16"
 export NODE_PORT_RANGE="8400-9000"
 
 # 集群master IP 数组
-export MASTER_IPS=(192.168.6.211 192.168.6.212 192.168.6.213)
+export MASTER_IPS=(192.168.0.11 192.168.0.12 192.168.0.13)
 
 # 集群master 对应的 主机名数组
 export MASTER_NAMES=(kube-master1 kube-master2 kube-master3)
 
 # 集群node IP 数组
-export NODE_IPS=(192.168.6.221 192.168.6.222 192.168.6.223)
+export NODE_IPS=(192.168.0.21 192.168.0.22 192.168.0.23)
 
 # 集群node IP 对应的 主机名数组
 export NODE_NAMES=(kube-node1 kube-node2 kube-node3)
 
 # 集群master_node IP 数组（方便分发）
-export MASTER_NODE_IPS=(192.168.6.211 192.168.6.212 192.168.6.213 192.168.6.221 192.168.6.222 192.168.6.223)
+export MASTER_NODE_IPS=(192.168.0.11 192.168.0.12 192.168.0.13 192.168.0.21 192.168.0.22 192.168.0.23)
 
 # kube-apiserver 的 VIP（HA 组件 keepalived 发布的 IP）
-export MASTER_VIP=192.168.6.210
+export MASTER_VIP=192.168.0.10
 
 # kube-apiserver VIP 地址（HA 组件 haproxy 监听 8443 端口）
 export KUBE_APISERVER="https://${MASTER_VIP}:8443"
@@ -41,9 +41,9 @@ export VIP_IF="ens33"
 # etcd名字
 export ETCD_NAMES=(etcd1 etcd2 etcd3) 
 # etcd集群服务地址列表
-export ETCD_ENDPOINTS="https://192.168.6.211:2379,https://192.168.6.212:2379,https://192.168.6.213:2379"
+export ETCD_ENDPOINTS="https://192.168.0.11:2379,https://192.168.0.12:2379,https://192.168.0.13:2379"
 # etcd集群间通信的 IP 和端口
-export ETCD_NODES="etcd1=https://192.168.6.211:2380,etcd2=https://192.168.6.212:2380,etcd3=https://192.168.6.213:2380"
+export ETCD_NODES="etcd1=https://192.168.0.11:2380,etcd2=https://192.168.0.12:2380,etcd3=https://192.168.0.13:2380"
 
 # flanneld 网络配置前缀
 export FLANNEL_ETCD_PREFIX="/kubernetes/network"
