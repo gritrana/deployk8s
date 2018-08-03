018e6bf597ff7357ed955bcb00764bc7163e0275
xujintao <xujintao@126.com>
2018-08-03 23:23:34 +0800

    use cp instead mv

--- deployenv.sh
+++ deployenv.sh
@@ -3,30 +3,31 @@ source env.sh
 # 设置dev环境
 echo "========解压cfssl========="
 # curl -O https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
-sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
+sudo cp cfssl_linux-amd64 /usr/local/bin/cfssl
 # curl -O https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
-sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+sudo cp cfssljson_linux-amd64 /usr/local/bin/cfssljson
 # curl -O https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
-sudo mv cfssl-certinfo_linux-amd64 /usr/local/bin/cfssl-certinfo
-if [ $? -ne 0 ];then exit; if
+sudo cp cfssl-certinfo_linux-amd64 /usr/local/bin/cfssl-certinfo
+if [ $? -ne 0 ];then exit; fi
+sudo chmod +x /usr/local/bin/cfssl*
 
 echo "========解压etcd v3.3.8========="
 # curl -O https://github.com/coreos/etcd/releases/download/v3.3.8/etcd-v3.3.8-linux-amd64.tar.gz
 tar -xzvf etcd-v3.3.8-linux-amd64.tar.gz
-if [ $? -ne 0 ];then exit; if
+if [ $? -ne 0 ];then exit; fi
 
 echo "========解压flannel v0.10.0========="
 #curl -O https://github.com/coreos/flannel/releases/download/v0.10.0/flannel-v0.10.0-linux-amd64.tar.gz
 mkdir flannel
 tar -xzvf flannel-v0.10.0-linux-amd64.tar.gz -C flannel
-if [ $? -ne 0 ];then exit; if
+if [ $? -ne 0 ];then exit; fi
 
 echo "========解压kubernetes v1.11.0"
 # 要使用其它版本的kubernetes 请到https://github.com/kubernetes/kubernetes/releases 查看
 # 对应的版本的CHANGELOG.md. 找到对应的链接下载
 # curl -O https://dl.k8s.io/v1.11.0/kubernetes-server-linux-amd64.tar.gz
 tar -xzvf kubernetes-server-linux-amd64.tar.gz
-if [ $? -ne 0 ];then exit; if
+if [ $? -ne 0 ];then exit; fi
 
 # 设置master机器环境
 echo "=========设置master机器环境========="
