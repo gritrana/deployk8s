bd4172c5f41bd55467086a5524a4987465aea2fd
xujintao <xujintao@126.com>
2018-08-06 19:26:12 +0800

    sed cluster hosts

--- deployenv.sh
+++ deployenv.sh
@@ -60,12 +60,16 @@ for ((i=0; i<3; i++))
     echo "修改hosts"
     for ((j=0; j<3; j++))
       do
-        echo "修改hosts，追加${MASTER_IPS[j]} ${MASTER_NAMES[j]}"
-        ssh root@${MASTER_IPS[i]} \
-          "echo '${MASTER_IPS[j]} ${MASTER_NAMES[j]}' >> /etc/hosts"
-#          "if [ -z `awk '/$MASTER_IPS[j] $MASTER_NAMES[j]/' /etc/hosts` ];then
-#           echo '${MASTER_IPS[j]} ${MASTER_NAMES[j]}' >> /etc/hosts
-#           fi"
+        ssh root@${MASTER_IPS[i]} "
+          if [ \"\`sed -n -e '/${MASTER_IPS[j]}\s\+${MASTER_NAMES[j]}/p' \
+            /etc/hosts\`\" ]
+          then
+            echo 'skip'
+          else
+            echo '追加${MASTER_IPS[j]} ${MASTER_NAMES[j]}'
+            echo '${MASTER_IPS[j]} ${MASTER_NAMES[j]}' >> /etc/hosts
+          fi
+          "
       done
   done
 
@@ -78,9 +82,16 @@ for ((i=0; i<3; i++))
     echo "修改hosts"
     for ((j=0; j<3; j++))
       do
-        echo "修改hosts，追加${NODE_IPS[j]} ${NODE_NAMES[j]}"
-        ssh root@${NODE_IPS[i]} \
-          "echo '${NODE_IPS[j]} ${NODE_NAMES[j]}' >> /etc/hosts"
+        ssh root@${NODE_IPS[i]} "
+          if [ \"\`sed -n -e '/${NODE_IPS[j]}\s\+${NODE_NAMES[j]}/p' \
+            /etc/hosts\`\" ]
+          then
+            echo 'skip'
+          else
+            echo '追加${NODE_IPS[j]} ${NODE_NAMES[j]}'
+            echo '${NODE_IPS[j]} ${NODE_NAMES[j]}' >> /etc/hosts
+          fi
+          "
       done
   done
 
